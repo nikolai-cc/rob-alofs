4eb144c7e819cf06c523a7ea2c31f5c3764b91aa
Nikolai <nikolai@theministry.co>
2022-06-28 16:01:52 +0200

    Update .eleventy.js
    remove pathPrefix for custom domain

--- .eleventy.js
+++ .eleventy.js
@@ -21,10 +21,10 @@ module.exports = function(eleventyConfig) {
   // Watch targets
   eleventyConfig.addWatchTarget("./src/styles/");
 
-  var pathPrefix = "";
-  if (process.env.GITHUB_REPOSITORY) {
-    pathPrefix = process.env.GITHUB_REPOSITORY.split('/')[1];
-  }
+  var pathPrefix = "/";
+//   if (process.env.GITHUB_REPOSITORY) {
+//     pathPrefix = process.env.GITHUB_REPOSITORY.split('/')[1];
+//   }
 
   return {
     dir: {
